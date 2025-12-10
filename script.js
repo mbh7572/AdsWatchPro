9fe7a71fc26cea3e95507f6e067bf183728ca827
Husnaindar <husnaindar9009@gmail.com>
2025-12-10 13:53:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+function redirectToNextPage(url) {
+  window.location.href = url;
+}
+function watchAd() {
+  // Code to watch ad and earn coins
+  console.log('Watching ad...');
+}
+
+function viewAd() {
+  // Code to view ad and earn coins
+  console.log('Viewing ad...');
+}
+
+function dailyReward() {
+  // Code to claim daily reward
+  console.log('Claiming daily reward...');
+}
